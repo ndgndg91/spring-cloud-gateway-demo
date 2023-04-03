de08b395ee86399ee61facfcb454222f1a0fba4d
ndgndg91 <ndgndg91@gmail.com>
2023-04-04 00:18:18 +0900

    reactive redis dependency

--- spring-cloud-gateway/build.gradle.kts
+++ spring-cloud-gateway/build.gradle.kts
@@ -25,6 +25,7 @@ extra["springCloudVersion"] = "2021.0.4"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")

f1f6770b5a5167725acce1263c039d42f2fa01b3
ndgndg91 <ndgndg91@gmail.com>
2023-04-13 22:51:08 +0900

    <refactor> change service name

--- account-service/src/main/kotlin/com/ndgndg91/account/AccountServiceApplication.kt
+++ account-service/src/main/kotlin/com/ndgndg91/account/AccountServiceApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SecondDepthServiceApplication
+class AccountServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<SecondDepthServiceApplication>(*args)
+    runApplication<AccountServiceApplication>(*args)
 }
